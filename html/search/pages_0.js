5fca9077f5461d40bd6684419a417e6edb443ba1
Brian Jackson <bjack205@gmail.com>
2022-07-31 15:23:16 -0400

    Update documentation

---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cocoa_3a_20a_20convex_20optimal_2dcontrol_20solver_20using_20primal_2ddual_20augmented_20lagrangian_1',['COCOA: A Convex Optimal-Control solver using primal-dual Augmented Lagrangian',['../index.html',1,'']]]
+];
