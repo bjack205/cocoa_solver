0a2d68a74088033fa1c4e22873ee4c9043486806
Brian Jackson <bjack205@gmail.com>
2022-07-31 09:10:56 -0400

    Add basic structure and deps

---
+++ src/main.c
@@ -0,0 +1,11 @@
+//
+// Created by Brian Jackson on 7/31/22.
+// Copyright (c) 2022 Robotic Exploration Lab. All rights reserved.
+//
+#include <stdio.h>
+
+#include <qdldl.h>
+
+int main() {
+  printf("Hello from cocoa solver!\n");
+}
