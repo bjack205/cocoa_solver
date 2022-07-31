286b30259ccfd1e1b94b81c4fdc219510bd69683
Brian Jackson <bjack205@gmail.com>
2022-07-31 10:48:12 -0400

    Add testing framework

---
+++ test/basic_test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Brian Jackson on 7/31/22.
+// Copyright (c) 2022 Robotic Exploration Lab. All rights reserved.
+//
+
+#include <gtest/gtest.h>
+#include <fmt/core.h>
+
+TEST(BasicTests, Addition) {
+  EXPECT_EQ(1, 2 - 1);
+}
