f246307206968d027bfa8716fdda2c2a268e26a2
Brian Jackson <bjack205@gmail.com>
2022-08-15 15:38:11 -0400

    Add precision as compile-time option

--- src/cocoa_types.h
+++ src/cocoa_types.h
@@ -5,8 +5,16 @@
 
 #pragma once
 
-#ifdef COCOA_SINGLE_PRECISON
+#ifdef COCOA_SINGLE_PRECISION
 typedef float cocoa_float;
+#define COCOA_USE_SINGLE_PRECISION 1
 #else
 typedef double cocoa_float;
+#define COCOA_USE_SINGLE_PRECISION 0
 #endif
+
+/**
+ * @brief Check if cocoa is running single precision arithmetic
+ * @return true if cocoa was compiled to use single precision floats, false if double.
+ */
+bool cocoa_use_single_precision(void) { return COCOA_USE_SINGLE_PRECISION; }
